c40010dea7125fe4662b211bc03aa0c82a68f121
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-29 17:24:11 -0500

    Update and rename bdoubleo100-common.js to MEE6.js

---
+++ server/cards/card-plugins/hermits/MEE6.js
@@ -0,0 +1,30 @@
+import HermitCard from './_hermit-card'
+
+class MEE6CharacterCard extends CharacterCard {
+	constructor() {
+		super({
+			id: 'MEE6',
+			name: 'MEESEXY',
+			rarity: 'common',
+			characterType: 'bot',
+			health: 280,
+			primary: {
+				name: 'Play Jams',
+				cost: ['bot'],
+				damage: 30,
+				power: null,
+			},
+			secondary: {
+				name: 'Level Up',
+				cost: ['bot', 'bot','bot'],
+				damage: 100,
+				power: null,
+			},
+		})
+	}
+// done
+	register(game) {}
+}
+
+export default MEE6CharacterCard
+
