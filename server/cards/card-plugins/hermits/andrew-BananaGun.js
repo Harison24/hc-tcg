f57c8379a9738ce955b3e84820f7e2ebdaf925d9
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-29 17:25:55 -0500

    Update and rename cubfan135-common.js to andrew-BananaGun.js

---
+++ server/cards/card-plugins/hermits/andrew-BananaGun.js
@@ -0,0 +1,29 @@
+import HermitCard from './_hermit-card'
+
+class BananaGunCharacterCard extends HermitCard {
+	constructor() {
+		super({
+			id: 'andrew-BananaGun',
+			name: 'Banana Gun',
+			rarity: 'common',
+			characterType: 'cat',
+			health: 290,
+			primary: {
+				name: 'Banana Blast',
+				cost: ['cat'],
+				damage: 50,
+				power: null,
+			},
+			secondary: {
+				name: 'School Shooting',
+				cost: ['cat', 'cat','any'],
+				damage: 90,
+				power: null,
+			},
+		})
+	}
+
+	register(game) {}
+}
+
+export default BananaGunCharacterCard
