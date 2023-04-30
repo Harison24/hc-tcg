822b07bce45f47780dc26dbeba5ca3bd385895d5
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-29 19:30:56 -0500

    Update index.js

--- server/cards/card-plugins/effects/index.js
+++ server/cards/card-plugins/effects/index.js
@@ -1,31 +1,13 @@
 import FireTruckEffectCard from './fire-truck'
 import DrMarioEffectCard from './dr-mario'
-import ShieldEffectCard from './shield'
 import RubberChickenEffectCard from './rubber-chicken'
 import WallOfFruitEffectCard from './wall-of-fruit'
-import DiamondArmorEffectCard from './diamond-armor'
-import NetheriteArmorEffectCard from './netherite-armor'
-import WolfEffectCard from './wolf'
-import TotemEffectCard from './totem'
-import BedEffectCard from './bed'
-import ThornsEffectCard from './thorns'
-import MendingEffectCard from './mending'
-import LoyaltyEffectCard from './loyalty'
 
 const EFFECT_CARDS = [
-	new BedEffectCard(),
-	new WolfEffectCard(),
 	new DrMarioEffectCard(),
 	new WallOfFruitEffectCard(),
 	new RubberChickenEffectCard(),
-	new ShieldEffectCard(),
 	new FireTruckEffectCard(),
-	new DiamondArmorEffectCard(),
-	new NetheriteArmorEffectCard(),
-	new TotemEffectCard(),
-	new ThornsEffectCard(),
-	new MendingEffectCard(),
-	new LoyaltyEffectCard(),
 ]
 
 export default EFFECT_CARDS
