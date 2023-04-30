81ede8f9f17648b70a3a698a1272b35c89615689
Harison24 <126421022+Harison24@users.noreply.github.com>
2023-04-29 22:17:07 -0500

    add trench types

--- server/const/strengths.js
+++ server/const/strengths.js
@@ -4,16 +4,13 @@
 
 /** @type {Record<HermitTypeT, Array<HermitTypeT>>} */
 const Strengths = {
-	balanced: [],
-	builder: ['terraform'],
-	explorer: ['builder'],
-	farm: ['explorer'],
-	miner: ['prankster', 'redstone'],
-	prankster: ['pvp', 'builder'],
-	pvp: ['speedrunner', 'farm'],
-	redstone: ['pvp'],
-	speedrunner: ['miner', 'prankster'],
-	terraform: ['redstone'],
+	australian: ['bacon'],
+	bacon: ['iceCream'],
+	bot: ['minecraft'],
+	cat: ['bot','iceCream'],
+	iceCream: ['toddler', 'bot'],
+	toddler: ['australian', 'cat'],
+	minecraft: ['toddler','australian'],
 }
 
 export default Strengths
